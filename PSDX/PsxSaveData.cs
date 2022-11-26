43ff07eaa20d49f4f49d4343dd7c6ee7a8ae4be0
giuse94 <59248203+giuse94@users.noreply.github.com>
2022-11-26 10:59:52 +0100

    Initial commit
    * Solution and project files.
    * First version of PsxSaveData class.

---
+++ PSDX/PsxSaveData.cs
@@ -0,0 +1,22 @@
+﻿namespace PSDX;
+
+public class PsxSaveData
+{
+    private const int _saveDataLength = 8320; // 8192 (1 block) + 128 (header sector) bytes.
+
+    private MemoryStream _stream = new(_saveDataLength);
+
+    // Return a copy so that consumers won't interfere.
+    private MemoryStream Stream => new MemoryStream(_stream.ToArray());
+
+    public PsxSaveData(Stream s)
+    {
+        if (s == null) throw new ArgumentNullException(nameof(s));
+        if (s.Length != _saveDataLength)
+        {
+            throw new ArgumentException($"The size of Single Save Format files (.MCS) must be {_saveDataLength} bytes.", nameof(s));
+        }
+        // Copy the stream so that we won't change the original data.
+        s.CopyTo(_stream);
+    }
+}
