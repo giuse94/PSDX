78d615c0c29e44db55746ec306cb1d8c4174f728
giuse94 <59248203+giuse94@users.noreply.github.com>
2022-12-04 15:57:29 +0100

    Add GetFileName() method

--- PSDX/PsxSaveData.cs
+++ PSDX/PsxSaveData.cs
@@ -4,6 +4,10 @@ public class PsxSaveData
 {
     private const int _saveDataLength = 8320; // 8192 (1 block) + 128 (header sector) bytes.
 
+    private const int _fileNameOffset = 0xA;
+
+    private const int _fileNameLength = 20; // 20 bytes, discarding the null-terminator.
+
     private MemoryStream _stream = new(_saveDataLength);
 
     // Return a copy so that consumers won't interfere.
@@ -18,5 +22,13 @@ public class PsxSaveData
         }
         // Copy the stream so that we won't change the original data.
         s.CopyTo(_stream);
+    }
+
+    public string GetFileName()
+    {
+        _stream.Position = _fileNameOffset;
+        byte[] buffer = new byte[_fileNameLength];
+        _stream.Read(buffer, 0, _fileNameLength);
+        return System.Text.Encoding.ASCII.GetString(buffer);
     }
 }
