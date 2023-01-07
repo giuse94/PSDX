bd3f2e847618b088bba29ca4e05cef9344f58f81
giuse94 <59248203+giuse94@users.noreply.github.com>
2023-01-07 17:02:00 +0100

    Simplify syntax and inline local variable

--- PSDX.Tests/PsdxTests.cs
+++ PSDX.Tests/PsdxTests.cs
@@ -7,11 +7,7 @@ public class PsdxTests
     [Fact]
     public void PsxSaveDataCtorThrowsAneWithNullStream()
     {
-        static void CodeToTest()
-        {
-            Stream? nullStream = null;
-            _ = new PsxSaveData(nullStream!); // Intentionally suppress the (right) warning.
-        }
+        static void CodeToTest() => _ = new PsxSaveData(null!); // Intentionally suppress the (right) warning.
 
         Assert.Throws<ArgumentNullException>(CodeToTest);
     }
