1c1e81c6e6db519e0a7cedfc2b1fec82ffd5f7cf
giuse94 <59248203+giuse94@users.noreply.github.com>
2023-02-12 09:50:12 +0100

    Management of progress, crystal and gem status

--- PSDX.Tests/PsdxTests.cs
+++ PSDX.Tests/PsdxTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using Xunit;
@@ -161,5 +162,219 @@ public class PsdxTests
         fs.Position = 0;
         fs.CopyTo(ms);
         Assert.True(originalData.SequenceEqual(ms.ToArray()));
+    }
+
+    [Theory]
+    [InlineData(-1), InlineData(0), InlineData(28)]
+    public void LevelRelatedMethodsThrowAoReWithWrongNumber(int level)
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        void GetProgressStatus() => cb2.GetProgressStatus(level);
+        void SetProgressStatus() => cb2.SetProgressStatus(level, true);
+        void GetCrystalStatus() => cb2.GetCrystalStatus(level);
+        void SetCrystalStatus() => cb2.SetCrystalStatus(level, true);
+        void GetGemStatus() => cb2.GetGemStatus(level, CrashBandicoot2SaveData.GemType.AllBoxesGem);
+        void SetGemStatus() => cb2.SetGemStatus(level, CrashBandicoot2SaveData.GemType.AllBoxesGem, true);
+
+        _ = Assert.Throws<ArgumentOutOfRangeException>(GetProgressStatus);
+        _ = Assert.Throws<ArgumentOutOfRangeException>(SetProgressStatus);
+        _ = Assert.Throws<ArgumentOutOfRangeException>(GetCrystalStatus);
+        _ = Assert.Throws<ArgumentOutOfRangeException>(SetCrystalStatus);
+        _ = Assert.Throws<ArgumentOutOfRangeException>(GetGemStatus);
+        _ = Assert.Throws<ArgumentOutOfRangeException>(SetGemStatus);
+    }
+
+    [Theory]
+    [InlineData(26), InlineData(27)]
+    public void CrystalRelatedMethodsThrowIoeWithWrongNumber(int level)
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        void GetCrystalStatus() => cb2.GetCrystalStatus(level);
+        void SetCrystalStatus() => cb2.SetCrystalStatus(level, true);
+
+        _ = Assert.Throws<InvalidOperationException>(GetCrystalStatus);
+        _ = Assert.Throws<InvalidOperationException>(SetCrystalStatus);
+    }
+
+    [Theory]
+    [InlineData(4), InlineData(5), InlineData(6), InlineData(8), InlineData(9), InlineData(13)]
+    [InlineData(15), InlineData(16), InlineData(22), InlineData(24), InlineData(26), InlineData(27)]
+    public void GemRelatedMethodsThrowIoeWithWrongNumber(int level)
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        void GetGemStatus() => cb2.GetGemStatus(level, CrashBandicoot2SaveData.GemType.SecondGem);
+        void SetGemStatus() => cb2.SetGemStatus(level, CrashBandicoot2SaveData.GemType.SecondGem, true);
+
+        _ = Assert.Throws<InvalidOperationException>(GetGemStatus);
+        _ = Assert.Throws<InvalidOperationException>(SetGemStatus);
+    }
+
+    [Theory]
+    [InlineData(0), InlineData(3)]
+    public void GemRelatedMethodsThrowIeAe(int enumIntValue)
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+        var enumValue = (CrashBandicoot2SaveData.GemType)enumIntValue;
+
+        void GetGemStatus() => cb2.GetGemStatus(1, enumValue);
+        void SetGemStatus() => cb2.SetGemStatus(1, enumValue, true);
+
+        _ = Assert.Throws<InvalidEnumArgumentException>(GetGemStatus);
+        _ = Assert.Throws<InvalidEnumArgumentException>(SetGemStatus);
+    }
+
+    [Fact]
+    public void GetProgressStatusReturnsTrueForLevel1()
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        bool traversed = cb2.GetProgressStatus(1);
+
+        Assert.True(traversed);
+    }
+
+    [Fact]
+    public void GetProgressStatusReturnsFalseForOtherLevels()
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        for (int level = 2; level < 28; level++)
+        {
+            bool traversed = cb2.GetProgressStatus(level);
+            Assert.False(traversed);
+        }
+    }
+
+    [Theory]
+    [InlineData(1, false), InlineData(1, true), InlineData(8, false), InlineData(8, true)]
+    [InlineData(14, false), InlineData(14, true), InlineData(22, false), InlineData(22, true)]
+    public void TestSetProgressStatus(int level, bool traversed)
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        cb2.SetProgressStatus(level, traversed);
+
+        Assert.Equal(traversed, cb2.GetProgressStatus(level));
+    }
+
+    [Fact]
+    public void GetCrystalStatusReturnsTrueForLevel1()
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        bool collected = cb2.GetCrystalStatus(1);
+
+        Assert.True(collected);
+    }
+
+    [Fact]
+    public void GetCrystalStatusReturnsFalseForOtherLevels()
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        for (int level = 2; level < 26; level++)
+        {
+            bool collected = cb2.GetCrystalStatus(level);
+            Assert.False(collected);
+        }
+    }
+
+    [Theory]
+    [InlineData(1, false), InlineData(1, true), InlineData(5, false), InlineData(5, true)]
+    [InlineData(9, false), InlineData(9, true), InlineData(18, false), InlineData(18, true)]
+    public void TestSetCrystalStatus(int level, bool collected)
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        cb2.SetCrystalStatus(level, collected);
+
+        Assert.Equal(collected, cb2.GetCrystalStatus(level));
+    }
+
+    [Fact]
+    public void GetAbGemStatusReturnsFalseForAllLevels()
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        for (int level = 1; level < 28; level++)
+        {
+            bool collected = cb2.GetGemStatus(level, CrashBandicoot2SaveData.GemType.AllBoxesGem);
+            Assert.False(collected);
+        }
+    }
+
+    [Fact]
+    public void Get2ndGemStatusReturnsFalseForAllLevels()
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        int[] levelsWithSecondGem = { 1, 2, 3, 7, 10, 11, 12, 14, 17, 18, 19, 20, 21, 23, 25 };
+        for (int i = 0; i < levelsWithSecondGem.Length; i++)
+        {
+            bool collected = cb2.GetGemStatus(levelsWithSecondGem[i], CrashBandicoot2SaveData.GemType.SecondGem);
+            Assert.False(collected);
+        }
+    }
+
+    [Theory]
+    [InlineData(1, CrashBandicoot2SaveData.GemType.AllBoxesGem, false), InlineData(1, CrashBandicoot2SaveData.GemType.AllBoxesGem, true)]
+    [InlineData(1, CrashBandicoot2SaveData.GemType.SecondGem, false), InlineData(1, CrashBandicoot2SaveData.GemType.SecondGem, true)]
+    [InlineData(10, CrashBandicoot2SaveData.GemType.AllBoxesGem, false), InlineData(10, CrashBandicoot2SaveData.GemType.AllBoxesGem, true)]
+    [InlineData(10, CrashBandicoot2SaveData.GemType.SecondGem, false), InlineData(10, CrashBandicoot2SaveData.GemType.SecondGem, true)]
+    [InlineData(19, CrashBandicoot2SaveData.GemType.AllBoxesGem, false), InlineData(19, CrashBandicoot2SaveData.GemType.AllBoxesGem, true)]
+    [InlineData(19, CrashBandicoot2SaveData.GemType.SecondGem, false), InlineData(19, CrashBandicoot2SaveData.GemType.SecondGem, true)]
+    public void TestSetGemStatus(int level, CrashBandicoot2SaveData.GemType gemType, bool collected)
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        cb2.SetGemStatus(level, gemType, collected);
+
+        Assert.Equal(collected, cb2.GetGemStatus(level, gemType));
+    }
+
+    [Fact]
+    public void FlagOnRemainsOn()
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        cb2.SetProgressStatus(1, true);
+        cb2.SetCrystalStatus(1, true);
+
+        Assert.True(cb2.GetProgressStatus(1));
+        Assert.True(cb2.GetCrystalStatus(1));
+    }
+
+    [Fact]
+    public void FlagOffRemainsOff()
+    {
+        using var fs = new FileStream("cb2.mcs", FileMode.Open);
+        var cb2 = new CrashBandicoot2SaveData(fs);
+
+        cb2.SetProgressStatus(2, false);
+        cb2.SetCrystalStatus(2, false);
+        cb2.SetGemStatus(2, CrashBandicoot2SaveData.GemType.AllBoxesGem, false);
+        cb2.SetGemStatus(2, CrashBandicoot2SaveData.GemType.SecondGem, false);
+
+        Assert.False(cb2.GetProgressStatus(2));
+        Assert.False(cb2.GetCrystalStatus(2));
+        Assert.False(cb2.GetGemStatus(2, CrashBandicoot2SaveData.GemType.AllBoxesGem));
+        Assert.False(cb2.GetGemStatus(2, CrashBandicoot2SaveData.GemType.SecondGem));
     }
 }
--- PSDX/PsxSaveData.cs
+++ PSDX/PsxSaveData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 
 namespace PSDX;
@@ -76,9 +77,61 @@ public class CrashBandicoot2SaveData : PsxSaveData
     /// </summary>
     private const string _serialNumber = "BESCES-00967";
 
+    private const int _maxLevelNumber = 27;
+
     private const int _checksumOffset = 0x1A4;
 
     private const int _akuAkuOffset = 0x1B4;
+
+    private const int _progressOffset = 0x1BC;
+
+    private const int _crystalsOffset = 0x1C4;
+
+    private const int _gemsOffset = 0x1CC;
+
+    /// <summary>
+    /// Flags and relative offsets of progress, crystal and all-boxes-gem for each level.
+    /// </summary>
+    private static readonly (byte Flag, byte Offset)[] _commonInfo = new (byte Flag, byte Offset)[_maxLevelNumber]
+    {
+        (0x40, 3), (0x40, 1), (0x02, 3), (0x80, 3), (0x01, 3),
+        (0x02, 2), (0x01, 4), (0x20, 3), (0x08, 3), (0x08, 4),
+        (0x02, 4), (0x04, 1), (0x04, 4), (0x40, 2), (0x80, 2),
+        (0x20, 1), (0x20, 2), (0x08, 2), (0x80, 1), (0x10, 4),
+        (0x01, 2), (0x04, 2), (0x10, 1), (0x04, 3), (0x40, 4),
+        (0x20, 4), (0x80, 4)
+    };
+
+    /// <summary>
+    /// Flags and relative offsets of the second gem for levels that have it.
+    /// </summary>
+    private static readonly (byte Flag, byte Offset)?[] _secondGemInfo = new (byte Flag, byte Offset)?[_maxLevelNumber]
+    {
+        (0x20, 7), (0x04, 7), (0x02, 0), null,      null,
+        null,      (0x04, 0), null,      null,      (0x08, 7),
+        (0x40, 7), (0x08, 0), null,      (0x10, 0), null,
+        null,      (0x01, 1), (0x02, 1), (0x02, 7), (0x10, 7),
+        (0x20, 0), null,      (0x40, 0), null,      (0x80, 0),
+        null,      null
+    };
+
+    /// <summary>
+    /// Represents the two types of gem available in the game.
+    /// </summary>
+    public enum GemType
+    {
+        /// <summary>
+        /// The gem obtained by destroying all the boxes in a level.<br/>
+        /// All levels in the game have this type of gem.
+        /// </summary>
+        AllBoxesGem = 1,
+
+        /// <summary>
+        /// The gem obtained by completing a level-specific task.<br/>
+        /// Not all levels in the game have this type of gem.
+        /// </summary>
+        SecondGem = 2
+    }
 
     /// <summary>
     /// Gets or sets a value indicating whether <see cref="GetStream"/> should compute the right checksum
@@ -196,5 +249,209 @@ public class CrashBandicoot2SaveData : PsxSaveData
         Stream.Position = _akuAkuOffset;
         byte[] bytes = BitConverter.GetBytes(number);
         Stream.Write(bytes);
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether the specified <paramref name="level"/> has been traversed, i.e. its exit has been reached.
+    /// </summary>
+    /// <param name="level">The number of the level to check for the progress status.</param>
+    /// <returns><see langword="true"/> if the level has been traversed, otherwise <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">The specified <paramref name="level"/> number is less than one or greater than twenty-seven.</exception>
+    public bool GetProgressStatus(int level)
+    {
+        if (level < 1 || level > _maxLevelNumber)
+        {
+            throw new ArgumentOutOfRangeException(nameof(level), level, "The specified level does not exist.");
+        }
+
+        (byte levelProgressFlag, byte levelProgressOffset) = _commonInfo[level - 1];
+        Stream.Position = _progressOffset + levelProgressOffset;
+        int progressFlag = Stream.ReadByte();
+        // We can't just return (progressFlag > 0) because different levels can share the same slot.
+        return (progressFlag & levelProgressFlag) > 0;
+    }
+
+    /// <summary>
+    /// Sets a value indicating whether the specified <paramref name="level"/> has been traversed, i.e. its exit has been reached.
+    /// </summary>
+    /// <param name="level">The number of the level to set the progress status of.</param>
+    /// <param name="traversed">Determines whether the level has been traversed.</param>
+    /// <exception cref="ArgumentOutOfRangeException">The specified level number is less than one or greater than twenty-seven.</exception>
+    public void SetProgressStatus(int level, bool traversed)
+    {
+        if (level < 1 || level > _maxLevelNumber)
+        {
+            throw new ArgumentOutOfRangeException(nameof(level), level, "The specified level does not exist.");
+        }
+
+        (byte levelProgressFlag, byte levelProgressOffset) = _commonInfo[level - 1];
+        int progressOffset = _progressOffset + levelProgressOffset;
+        Stream.Position = progressOffset;
+        int progressFlag = Stream.ReadByte();
+        if (traversed)
+        {
+            progressFlag |= levelProgressFlag;
+        }
+        else
+        {
+            progressFlag &= ~levelProgressFlag;
+        }
+
+        Stream.Position = progressOffset;
+        Stream.WriteByte((byte)progressFlag);
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether the crystal has been collected in the specified <paramref name="level"/>.
+    /// </summary>
+    /// <param name="level">The number of the level to check for the crystal status.</param>
+    /// <returns><see langword="true"/> if the crystal has been collected, otherwise <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">The specified <paramref name="level"/> number is less than one or greater than twenty-seven.</exception>
+    /// <exception cref="InvalidOperationException">The specified <paramref name="level"/> does not contain a crystal.</exception>
+    public bool GetCrystalStatus(int level)
+    {
+        if (level < 1 || level > _maxLevelNumber)
+        {
+            throw new ArgumentOutOfRangeException(nameof(level), level, "The specified level does not exist.");
+        }
+        if (level == 26 || level == 27)
+        {
+            throw new InvalidOperationException($"Level {level} does not contain a crystal.");
+        }
+
+        (byte levelCrystalFlag, byte levelCrystalOffset) = _commonInfo[level - 1];
+        Stream.Position = _crystalsOffset + levelCrystalOffset;
+        int crystalFlag = Stream.ReadByte();
+        return (crystalFlag & levelCrystalFlag) > 0;
+    }
+
+    /// <summary>
+    /// Sets a value indicating whether the crystal has been collected in the specified <paramref name="level"/>.
+    /// </summary>
+    /// <param name="level">The number of the level to set the crystal status of.</param>
+    /// <param name="collected">Determines whether the crystal has been collected in the level.</param>
+    /// <exception cref="ArgumentOutOfRangeException">The specified <paramref name="level"/> number is less than one or greater than twenty-seven.</exception>
+    /// <exception cref="InvalidOperationException">The specified <paramref name="level"/> does not contain a crystal.</exception>
+    public void SetCrystalStatus(int level, bool collected)
+    {
+        if (level < 1 || level > _maxLevelNumber)
+        {
+            throw new ArgumentOutOfRangeException(nameof(level), level, "The specified level does not exist.");
+        }
+        if (level == 26 || level == 27)
+        {
+            throw new InvalidOperationException($"Level {level} does not contain a crystal.");
+        }
+
+        (byte levelCrystalFlag, byte levelCrystalOffset) = _commonInfo[level - 1];
+        int crystalOffset = _crystalsOffset + levelCrystalOffset;
+        Stream.Position = crystalOffset;
+        int crystalFlag = Stream.ReadByte();
+        if (collected)
+        {
+            crystalFlag |= levelCrystalFlag;
+        }
+        else
+        {
+            crystalFlag &= ~levelCrystalFlag;
+        }
+
+        Stream.Position = crystalOffset;
+        Stream.WriteByte((byte)crystalFlag);
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether the specified gem type has been collected in the specified <paramref name="level"/>.
+    /// </summary>
+    /// <param name="level">The number of the level to check for the gem status.</param>
+    /// <param name="gemType">The type of gem to check the status of.</param>
+    /// <returns><see langword="true"/> if the gem has been collected, otherwise <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">The specified <paramref name="level"/> number is less than one or greater than twenty-seven.</exception>
+    /// <exception cref="InvalidOperationException">
+    /// <paramref name="gemType"/> is <see cref="GemType.SecondGem"/> and the specified <paramref name="level"/> does not contain that type of gem.
+    /// </exception>
+    /// <exception cref="InvalidEnumArgumentException">The specified <paramref name="gemType"/> is not a valid enum value.</exception>
+    public bool GetGemStatus(int level, GemType gemType)
+    {
+        if (level < 1 || level > _maxLevelNumber)
+        {
+            throw new ArgumentOutOfRangeException(nameof(level), level, "The specified level does not exist.");
+        }
+
+        (byte Flag, byte Offset) levelGemInfo;
+        if (gemType == GemType.AllBoxesGem)
+        {
+            levelGemInfo = _commonInfo[level - 1];
+        }
+        else if (gemType == GemType.SecondGem)
+        {
+            levelGemInfo = _secondGemInfo[level - 1].GetValueOrDefault();
+        }
+        else
+        {
+            throw new InvalidEnumArgumentException(nameof(gemType), (int)gemType, typeof(GemType));
+        }
+
+        if (levelGemInfo == default)
+        {
+            throw new InvalidOperationException($"Level {level} does not contain the second gem.");
+        }
+
+        Stream.Position = _gemsOffset + levelGemInfo.Offset;
+        int gemFlag = Stream.ReadByte();
+        return (gemFlag & levelGemInfo.Flag) > 0;
+    }
+
+    /// <summary>
+    /// Sets a value indicating whether the specified gem type has been collected in the specified <paramref name="level"/>.
+    /// </summary>
+    /// <param name="level">The number of the level to set the gem status of.</param>
+    /// <param name="gemType">The type of gem to set the status of.</param>
+    /// <param name="collected">Determines whether the gem has been collected in the level.</param>
+    /// <exception cref="ArgumentOutOfRangeException">The specified <paramref name="level"/> number is less than one or greater than twenty-seven.</exception>
+    /// <exception cref="InvalidOperationException">
+    /// <paramref name="gemType"/> is <see cref="GemType.SecondGem"/> and the specified <paramref name="level"/> does not contain that type of gem.
+    /// </exception>
+    /// <exception cref="InvalidEnumArgumentException">The specified <paramref name="gemType"/> is not a valid enum value.</exception>
+    public void SetGemStatus(int level, GemType gemType, bool collected)
+    {
+        if (level < 1 || level > _maxLevelNumber)
+        {
+            throw new ArgumentOutOfRangeException(nameof(level), level, "The specified level does not exist.");
+        }
+
+        (byte Flag, byte Offset) levelGemInfo;
+        if (gemType == GemType.AllBoxesGem)
+        {
+            levelGemInfo = _commonInfo[level - 1];
+        }
+        else if (gemType == GemType.SecondGem)
+        {
+            levelGemInfo = _secondGemInfo[level - 1].GetValueOrDefault();
+        }
+        else
+        {
+            throw new InvalidEnumArgumentException(nameof(gemType), (int)gemType, typeof(GemType));
+        }
+
+        if (levelGemInfo == default)
+        {
+            throw new InvalidOperationException($"Level {level} does not contain the second gem.");
+        }
+
+        int gemOffset = _gemsOffset + levelGemInfo.Offset;
+        Stream.Position = gemOffset;
+        int gemFlag = Stream.ReadByte();
+        if (collected)
+        {
+            gemFlag |= levelGemInfo.Flag;
+        }
+        else
+        {
+            gemFlag &= ~levelGemInfo.Flag;
+        }
+
+        Stream.Position = gemOffset;
+        Stream.WriteByte((byte)gemFlag);
     }
 }
